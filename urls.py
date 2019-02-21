a3129d2a3f41732cb5b8e69e79eb90aa23f5ac5c
Aditya Anand Thakur <anandaditya444@gmail.com>
2019-02-21 21:19:11 +0530

    endpoint to api

--- urls.py
+++ urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     url(r'^face_detection/detect/$', 'face_detector.views.detect'),
 
     # url(r'^$', 'cv_api.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    # url(r'^blog/', include('blog.urls')), 
 
     url(r'^admin/', include(admin.site.urls)),
 )
